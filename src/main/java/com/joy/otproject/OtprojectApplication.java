2028df5dd58aad1eaf9b4ee4a9ab983a10a9bc0b
yeseul <sseul0701@argonet.co.kr>
2022-11-18 17:15:19 +0900

    commit

---
+++ src/main/java/com/joy/otproject/OtprojectApplication.java
@@ -0,0 +1,13 @@
+package com.joy.otproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joy/otproject/OtprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joy.otproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
